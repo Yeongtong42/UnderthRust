ba463b7f6f887109df25c31071650671504d4bd8
iphuck22 <eldeshue@naver.com>
2025-05-11 03:02:02 +0900

    feat(binary_heap) : define basic structure
    binary heap으로 구성된 MinHeap의 구현을 위해서 다음을 정의
    - Comparator trait : 우선순위큐의 원소를 임의 순서로 정렬하기 위한 비교 기준
    - DefaultComparator : Ord trait을 구현한 T에 대해 기본적으로 제공되는 Comparator 구현체
    - MinHeap : 우선순위 큐 자료구조
    현현

--- collections/binary_heap/src/lib.rs
+++ collections/binary_heap/src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+
+#![allow(unused_macros)]
+#![allow(dead_code)]
+
+pub trait Comparator<T> {
+	fn compare(&self, a : &T, b : &T) -> std::cmp::Ordering;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Default)]
+pub struct DefaultComparator;
+impl<T:Ord> Comparator<T> for DefaultComparator {
+	#[inline]
+	fn compare(&self, a: &T, b:&T) -> std::cmp::Ordering {
+		a.cmp(b)
+	}
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct MinHeap<T : Ord, C: Comparator<T>> {
+	data: Vec<T>,
+	comparator: C,
 }
