b4387188fdd4bf31dbe6ea228696ebaeba1127d2
iphuck22 <eldeshue@naver.com>
2025-05-11 03:12:25 +0900

    feat : add index helper function
    heap구현 과정에서 부모/자식 노드를 구하는 함수 추가
    모든 인덱스는 0-based
    
    inline 바람, 가능하면 C의 매크로 함수가 적합함

--- collections/binary_heap/src/lib.rs
+++ collections/binary_heap/src/lib.rs
@@ -19,3 +19,21 @@ pub struct MinHeap<T, C: Comparator<T>> {
 	data: Vec<T>,
 	comparator: C,
 }
+
+impl<T, C> MinHeap<T, C>
+where
+	C : Default + Comparator<T>
+{
+	#[inline]
+	fn get_parent(i : usize) -> usize {
+		((i + 1) >> 1) - 1
+	}
+	#[inline]
+	fn get_left(i : usize) -> usize {
+		((i + 1) << 1) - 1
+	}
+	#[inline]
+	fn get_right(i : usize) -> usize {
+		((i + 1) << 1) + 1 - 1
+	}
+}
