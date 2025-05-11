1a654433f5d1707e16236ff319bd933aa8efcd3c
iphuck22 <eldeshue@naver.com>
2025-05-11 15:59:11 +0900

    chore : add comment

--- collections/binary_heap/src/lib.rs
+++ collections/binary_heap/src/lib.rs
@@ -4,11 +4,13 @@
 //! 기존에 prelude에 Max heap이 있으므로, min heap으로 구현한다.
 //!
 //! # Implementation
-//! ## Traits
+//! ## Related Traits
 //! ### Ord
-//! heap tree 내에서 데이터의 순서를 얻기 위한 비교 방법을 표현한 trait
-//! 비교는 std::cmp로 이루어지며, Ordering 이라는 enum을 반환함
+//! 데이터의 비교 방법을 정의한 trait
+//! 비교는 std::cmp로 이루어지며, Ordering enum을 반환함
 //! variant로는 Greater, Equal, Less의 3종이 존재
+//!
+//! Ord가 구현되어 있다면, 이를 활용하는 DefaultComparator를 사용가능
 //!
 //! ### comparator
 //! Ord trait이 없는 T, 혹은 기존의 Ord와는 다른 기준으로 정렬을 위해 도입한 trait
@@ -21,7 +23,7 @@
 //! ### T
 //! 실제 저장하게 될 데이터의 타입
 //! ### C
-//! Comparator trait을 구현한 임의의 객체
+//! Comparator trait을 구현한 임의 타입
 //!
 //! ## Field
 //! ### data - Vec<T>
@@ -65,24 +67,31 @@ impl<T, C> MinHeap<T, C>
 where
 	C : Comparator<T>
 {
-	/// note : get_parent(0) is usize::max, will always be bigger than data.len()
 	/// TODO : change it to macro function
+
+	/// helper function for heap tree, get index of parent node
+	/// note : get_parent(0) is usize::max, will always be bigger than data.len()
 	#[inline]
 	fn get_parent(i : usize) -> usize {
 		((i + 1) >> 1).wrapping_sub(1)
 	}
+
+	/// helper function for heap tree, get index of left child node
 	#[inline]
 	fn get_left(i : usize) -> usize {
 		((i + 1) << 1) - 1
 	}
+
+	/// helper function for heap tree, get index of right child node
 	#[inline]
 	fn get_right(i : usize) -> usize {
 		((i + 1) << 1) + 1 - 1
 	}
 
-	/// # Description
-	/// keep invariant for heap tree
+	/// keep order of heap tree
+	/// child nodes are bigger than it's parent node
 	/// for performace reason, comp.compare()'s inlining is crucial
+	/// O(log n)
 	fn min_heapify(data : &mut Vec<T>, comp : &impl Comparator<T>, i : usize) {
 		let l = Self::get_left(i);
 		let r = Self::get_right(i);
@@ -99,6 +108,8 @@ where
 		}
 	}
 
+	/// reorder vector to make heap tree
+	/// O(n)
 	fn build_heap(data : &mut Vec<T>, comp : &impl Comparator<T>) {
 		let offset = data.len() / 2;
 		for i in (0..offset).rev() {
@@ -106,6 +117,7 @@ where
 		}
 	}
 
+	/// create empty min heap
 	pub fn new(comp : C) -> MinHeap<T, C> {
 		MinHeap {
 			data : Vec::new(),
@@ -113,6 +125,7 @@ where
 		}
 	}
 
+	/// create min heap with vector
 	pub fn from_vec(mut vec : Vec<T>, comp : C) -> MinHeap<T, C> {
 		let comparator = comp;
 		Self::build_heap(&mut vec, &comparator);
@@ -122,6 +135,8 @@ where
 		}
 	}
 
+	/// push new element to the heap
+	/// O(log n)
 	pub fn push(&mut self, elem : T) {
 		let data = &mut self.data;
 		data.push(elem);
@@ -137,12 +152,16 @@ where
 		}
 	}
 
+	/// add multiple element to the heap
+	/// O(n)
 	pub fn extend(&mut self, elems : &mut Vec<T>) {
 		let data = &mut self.data;
 		data.append(elems);
 		Self::build_heap(data,&self.comparator);
 	}
 
+	/// extract ownership of the element at root
+	/// O(log n)
 	pub fn pop(&mut self) -> Option<T> {
 		let data = &mut self.data;
 		let end_idx = data.len() - 1;
