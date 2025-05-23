8e8bfbcb4544d569770f47aebae9abc8fb206279
iphuck22 <eldeshue@naver.com>
2025-05-23 18:38:45 +0900

    test : update integration test.
    1000_000 was too big to test insertion_sort, which is O(N^2).

--- algorithms/sort/tests/integration_test.rs
+++ algorithms/sort/tests/integration_test.rs
@@ -7,12 +7,18 @@ use rand::distr::StandardUniform;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
+const INSERTION_TEST_SIZE: usize = 1000;
+const TEST_SIZE: usize = 100_000;
+
 #[test]
 fn test_insertion_sort() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng
+        .sample_iter(StandardUniform)
+        .take(INSERTION_TEST_SIZE)
+        .collect();
 
     insertion_sort(&mut vec);
 
@@ -24,7 +30,10 @@ fn test_insertion_sort_by() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng
+        .sample_iter(StandardUniform)
+        .take(INSERTION_TEST_SIZE)
+        .collect();
 
     insertion_sort_by(&mut vec, |a: &i32, b: &i32| Reverse(a).cmp(&Reverse(b)));
 
@@ -36,7 +45,7 @@ fn test_merge_sort() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     merge_sort(&mut vec);
 
@@ -48,7 +57,7 @@ fn test_merge_sort_by() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     merge_sort_by(&mut vec, |a: &i32, b: &i32| Reverse(a).cmp(&Reverse(b)));
 
@@ -60,7 +69,7 @@ fn test_binary_quick_sort() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     binary_quick_sort(&mut vec);
 
@@ -72,7 +81,7 @@ fn test_binary_quick_sort_by() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     binary_quick_sort_by(&mut vec, |a: &i32, b: &i32| Reverse(a).cmp(&Reverse(b)));
 
@@ -84,7 +93,7 @@ fn test_ternary_quick_sort() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     ternary_quick_sort(&mut vec);
 
@@ -96,7 +105,7 @@ fn test_ternary_quick_sort_by() {
     let seed: u64 = 42;
     let rng = StdRng::seed_from_u64(seed);
 
-    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(1000_000).collect();
+    let mut vec: Vec<i32> = rng.sample_iter(StandardUniform).take(TEST_SIZE).collect();
 
     ternary_quick_sort_by(&mut vec, |a: &i32, b: &i32| Reverse(a).cmp(&Reverse(b)));
 
