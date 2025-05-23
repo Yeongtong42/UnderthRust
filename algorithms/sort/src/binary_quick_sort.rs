741043343c158b5a3a4f60ed52656da6810725f4
iphuck22 <eldeshue@naver.com>
2025-05-24 05:37:33 +0900

    feat : implement basic quick sort

--- algorithms/sort/src/binary_quick_sort.rs
+++ algorithms/sort/src/binary_quick_sort.rs
@@ -1,14 +1,74 @@
 #![allow(unused)]
 
+fn partition<T: Ord>(slice: &mut [T]) -> usize {
+    let len = slice.len();
+    let pivot = len - 1;
+    let mut cur_left_pos = 0usize;
+
+    for i in 0..len {
+        if slice[i] <= slice[pivot] {
+            slice.swap(cur_left_pos, i);
+            cur_left_pos += 1;
+        }
+    }
+    cur_left_pos - 1
+}
+
 pub fn binary_quick_sort<T: Ord>(slice: &mut [T]) {
-    // implement basic quick sort
+    let len = slice.len();
+    if len <= 1 {
+        return;
+    }
+
+    // partition
+    let pivot_pos = partition(slice);
+
+    // recurse two part
+    binary_quick_sort(&mut slice[0..pivot_pos]);
+    binary_quick_sort(&mut slice[pivot_pos + 1..len]);
+}
+
+fn partition_by<T, F>(slice: &mut [T], comp: F) -> (usize, F)
+where
+    F: FnMut(&T, &T) -> std::cmp::Ordering,
+{
+    use std::cmp::Ordering as O;
+    let mut cmp = comp;
+    let len = slice.len();
+    let pivot = len - 1;
+    let mut cur_left_pos = 0usize;
+
+    for i in 0..len {
+        if O::Greater != cmp(&slice[i], &slice[pivot]) {
+            slice.swap(cur_left_pos, i);
+            cur_left_pos += 1;
+        }
+    }
+    (cur_left_pos - 1, cmp)
+}
+
+fn quick_sort_by<T, F>(slice: &mut [T], comp: F) -> F
+where
+    F: FnMut(&T, &T) -> std::cmp::Ordering,
+{
+    let len = slice.len();
+    if len <= 1 {
+        return comp;
+    }
+
+    // partition
+    let (pivot_pos, comp) = partition_by(slice, comp);
+
+    // recurse two part
+    let comp = quick_sort_by(&mut slice[0..pivot_pos], comp);
+    quick_sort_by(&mut slice[pivot_pos + 1..len], comp)
 }
 
 pub fn binary_quick_sort_by<T, F>(slice: &mut [T], comp: F)
 where
     F: FnMut(&T, &T) -> std::cmp::Ordering,
 {
-    // implement basic quick sort by comp
+    quick_sort_by(slice, comp);
 }
 
 #[cfg(test)]
