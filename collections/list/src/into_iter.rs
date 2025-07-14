849e1c6fb8b9ca1ec0a2870abde537d8bc4de3bb
iphuck22 <eldeshue@naver.com>
2025-07-14 15:16:44 +0900

    feat : implement into_iterator
    - Define IntoIterator
    - Implement Iterator for IntoIterator

---
+++ collections/list/src/into_iter.rs
@@ -0,0 +1,40 @@
+use super::List;
+use crate::node::*;
+use std::boxed::Box;
+use std::iter::Iterator;
+use std::ptr::NonNull;
+
+#[derive(Debug)]
+pub struct IntoIter<T> {
+    source: Option<NonNull<Node<T>>>,
+    len: usize,
+}
+
+impl<T> IntoIter<T> {
+    pub(super) fn new(list: List<T>) -> Self {
+        // move ownership of nodes to the iterator
+        let result = IntoIter {
+            source: list.head,
+            len: list.len,
+        };
+        // to prevent double free, make list empty
+        let mut list = list;
+        list.head = None;
+        list.tail = None;
+        list.len = 0;
+        result
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(cur_node) = self.source {
+            self.source = Node::get_next_node(cur_node);
+            self.len -= 1;
+            unsafe { Some(Box::from_raw(cur_node.as_ptr()).get_data()) } // consume node here
+        } else {
+            None
+        }
+    }
+}
