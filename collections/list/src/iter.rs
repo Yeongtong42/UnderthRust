33b78fbbc63906052842eb55073bdb6b0d6a7840
iphuck22 <eldeshue@naver.com>
2025-07-14 12:05:40 +0900

    feat : implement iterator for the List.
    - define struct Iter
    - implement Iterator trait for Iter
    
    ExactSize, DoubleEnded, Fused trait will be added later.

---
+++ collections/list/src/iter.rs
@@ -0,0 +1,36 @@
+use super::List;
+use crate::node::*;
+use std::iter::Iterator;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+#[derive(Debug)]
+pub struct Iter<'a, T> {
+    source: Option<NonNull<Node<T>>>,
+    len: usize,
+    phantom: PhantomData<&'a Node<T>>,
+}
+
+impl<'a, T> Iter<'a, T> {
+    pub(super) fn new(list: &'a List<T>) -> Self {
+        Iter {
+            source: list.head,
+            len: list.len,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(cur_node) = self.source {
+            let result = unsafe { Some(cur_node.as_ref().get_data_ref()) };
+            self.source = Node::get_next_node(cur_node);
+            self.len -= 1;
+            result
+        } else {
+            None
+        }
+    }
+}
